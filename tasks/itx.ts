6ac6e1dd1f9cdd3243377d43c5793375c7a7d6d4
Ming-der Wang <mingderwang@operamail.com>
2023-01-19 20:20:13 +0800

    signer Wallet

--- tasks/itx.ts
+++ tasks/itx.ts
@@ -1,11 +1,13 @@
 import { task } from "hardhat/config";
 
 const INFURA_KEY = process.env.INFURA_API_KEY;
+const YOUR_PRIVATE_KEY = process.env.PRIVATE_KEY_1;
 task("itx", "Prints the itx", async (_, { ethers }) => {
 
 const itx = new ethers.providers.InfuraProvider(
   'goerli', // or 'mainnet'
   INFURA_KEY
 )
-  console.log(itx);
+const signer = new ethers.Wallet(YOUR_PRIVATE_KEY, itx)
+  console.log(signer)
 });
