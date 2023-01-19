8bc429e5c41e1b87bec956968b9ac2e0df344248
Ming-der Wang <mingderwang@operamail.com>
2023-01-19 20:26:10 +0800

    npx hardhat itx-getBalance: Your current ITX balance is 9999738717978761190

--- tasks/itx.ts
+++ tasks/itx.ts
@@ -2,12 +2,17 @@ import { task } from "hardhat/config";
 
 const INFURA_KEY = process.env.INFURA_API_KEY;
 const YOUR_PRIVATE_KEY = process.env.PRIVATE_KEY_1;
-task("itx", "Prints the itx", async (_, { ethers }) => {
+task("itx-getBalance", "Prints the itx", async (_, { ethers }) => {
+
 
 const itx = new ethers.providers.InfuraProvider(
   'goerli', // or 'mainnet'
   INFURA_KEY
 )
 const signer = new ethers.Wallet(YOUR_PRIVATE_KEY, itx)
-  console.log(signer)
+const response = await itx.send('relay_getBalance', [signer.address])
+console.log(`Your current ITX balance is ${response.balance}`)
+
 });
+
+
